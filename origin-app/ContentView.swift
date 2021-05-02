ee40cbfb90a4adde8108787641732f76ab247ad2
lyonsan <ryoga.yajima@gmail.com>
2021-05-02 15:17:48 +0900

    Initial Commit

---
+++ origin-app/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  origin-app
+//
+//  Created by lyonsan on 2021/05/02.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ origin-app/origin_appApp.swift
@@ -0,0 +1,18 @@
+//
+//  origin_appApp.swift
+//  origin-app
+//
+//  Created by lyonsan on 2021/05/02.
+//  
+//
+
+import SwiftUI
+
+@main
+struct origin_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
